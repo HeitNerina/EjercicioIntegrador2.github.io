39c31bc4039db7ac9273dbf44ae8ddd84be17f2f
HeitNerina <110876136+HeitNerina@users.noreply.github.com>
2022-11-10 16:46:41 -0300

    Update script.js
    agregue un if para que el calculo siempre sea con la cantidad mayor a 0

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ function calculoTicket(){
     let cant=document.getElementById("cantidad").value;
     let cat=document.getElementById("categoria");
     let categoria = cat.value;
-    switch(categoria){
-        case "Estudiante": montoFinal=cant*(200-(200*80)/100); break;
-        case "Trainee": montoFinal=cant*(200-(200*50)/100); break;
-        case "Junior": montoFinal=cant*(200-(200*15)/100); break;
+    if(cant<=0){
+        console.log(La cantidad tiene que ser superior a 0. Reintente");
+    }else{
+        switch(categoria){
+            case "Estudiante": montoFinal=cant*(200-(200*80)/100); break;
+            case "Trainee": montoFinal=cant*(200-(200*50)/100); break;
+            case "Junior": montoFinal=cant*(200-(200*15)/100); break;
+        }
     }
     document.getElementById("ronly").value = montoFinal;
 }
